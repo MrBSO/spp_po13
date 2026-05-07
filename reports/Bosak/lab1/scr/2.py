570f4d1b8337f19b2d106fc75c20d943645aed28
MrBSoD <8244984@gmail.com>
2026-05-07 22:10:46 +0300

    feat:comleted task1

---
+++ reports/Bosak/lab1/scr/2.py
@@ -0,0 +1,17 @@
+def is_valid_brackets(s):
+    brackets = {')': '(', '}': '{', ']': '['}
+    stack = []
+    
+    for char in s:
+        if char in '({[':
+            stack.append(char)
+        elif char in ')}]':
+            if not stack or stack[-1] != brackets[char]:
+                print(False)
+                return
+            stack.pop()
+    
+    print(len(stack) == 0)
+
+s = input()
+is_valid_brackets(s)
